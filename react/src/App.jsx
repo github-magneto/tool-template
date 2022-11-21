75e9fd716167dc5f9a99cb13c50ad71aea0bb3ed
magneto <1735721264@qq.com>
2022-11-21 18:48:55 +0800

    x

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,16 +1,7 @@
 import React from 'react'
 
-import Music from './View.Page.Music'
-import Message from './View.Global.Message'
-import Spin from './View.Global.Spin'
-
 function App() {
-  return <>
-    <Message />
-    <Spin />
-
-    <Music />
-  </>
+  return null
 }
 
 export default App
--- react/src/utils.imitation.js
+++ react/src/utils.imitation.js
@@ -4,24 +4,7 @@ const ImitationINS = new Imitation()
 
 ImitationINS.state = {
   xhrLoading: false,
-  message: 0,
-
-  pressKeep: [],
-  pressUpdate: 0,
-
-  tooltip: false,
-  scale: 1,
-  volume: 0.5,
-  cover: false,
-
-  media: [],
-
-  console: 'Simple_piano',
-  bgmCollection: null,
-  bgm: null,
-
-  screenWidth: 0,
-  screenHeight: 0,
+  message: 0
 }
 
 export default ImitationINS
