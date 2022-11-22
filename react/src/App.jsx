6e54f43aeb61623cbf0c262d07ddd7285d681536
magneto <1735721264@qq.com>
2022-11-22 11:30:39 +0800

    x

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import { HashRouter, Route, Switch } from 'react-router-dom'
+
+import Message from './view/Component.Message'
+import Spin from './view/Component.Spin'
 
 function App() {
-  return null
+  return <HashRouter>
+    <Message />
+    <Spin />
+
+    <Switch>
+      <Route path='/' exact></Route>
+    </Switch>
+  </HashRouter>
 }
 
 export default App
